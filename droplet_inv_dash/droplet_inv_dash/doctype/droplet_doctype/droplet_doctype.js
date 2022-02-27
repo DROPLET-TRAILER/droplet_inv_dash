bd9fff82573e76cc5b228ed3f1720edfe4e220d4
frappe <frappe@ubuntu.vm>
2022-02-27 23:57:30 +0000

    added placeholder doctype to make icon show up

---
+++ droplet_inv_dash/droplet_inv_dash/doctype/droplet_doctype/droplet_doctype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, BCIT CST 2022 and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Droplet Doctype', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ droplet_inv_dash/droplet_inv_dash/doctype/droplet_doctype/test_droplet_doctype.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, BCIT CST 2022 and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestDropletDoctype(unittest.TestCase):
+	pass
