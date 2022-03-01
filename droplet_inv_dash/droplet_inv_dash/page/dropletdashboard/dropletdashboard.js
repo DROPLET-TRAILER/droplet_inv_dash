be0bc2a5af9ba49bc5fc13b9b5e163a22decb411
StevenR88 <sreid97@my.bcit.ca>
2022-03-01 03:49:13 +0000

    updated Workspace Tags

--- droplet_inv_dash/droplet_inv_dash/page/dropletdashboard/dropletdashboard.js
+++ droplet_inv_dash/droplet_inv_dash/page/dropletdashboard/dropletdashboard.js
@@ -1,32 +1,22 @@
 frappe.pages['dropletdashboard'].on_page_load = function(wrapper) {
-	new DropDash(wrapper);
+	let page = frappe.ui.make_app_page({
+		title: 'Droplet Dashboard',
+		parent: wrapper,
+		single_column: true
+	})
+
+	$(frappe.render_template(frappe.droplet_dash_page.body, this)).appendTo(this.page.main);
+
+	// Button Click Redirect
+	// page.set_primary_action('Start Dashboard', () => window.location.href = frappe.urllib.get_full_url("/special")
+	// , 'octicon octicon-plus');
+
+	// Automatic Redirect
+	window.location.href = frappe.urllib.get_full_url("/special");
 }
 
-// PAGE CONTENT
-DropDash = Class.extend({
-	init: function (wrapper) {
-		this.page = frappe.ui.make_app_page({
-			parent: wrapper,
-			title: 'Droplet Dashboard',
-			single_column: true
-		});
-		// Make Page
-		this.make();
-	},
-	//Make Page Function
-	make: function () {
-		// Grab the Class
-		let me = $(this);
-
-		// Push DOM element to page
-		$(frappe.render_template(frappe.droplet_dash_page.body, this)).appendTo(this.page.main);
-		window.location.href = frappe.urllib.get_full_url("/special");
-	}
-	// End of Class
-})
-
 // HTML Content
-let body = '<h1>Opening Dashboard...</h1>';
+let body = '<h1>Starting Dashboard...</h1>';
 frappe.droplet_dash_page =  {
 	body: body
 }
