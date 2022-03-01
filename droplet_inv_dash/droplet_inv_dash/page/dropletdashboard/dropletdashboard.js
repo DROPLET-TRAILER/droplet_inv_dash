1d863ad17c41848c8a9534ff8f7190157f3b005a
frappe <frappe@ubuntu.vm>
2022-03-01 03:17:46 +0000

    added a redirect to special.html

--- droplet_inv_dash/droplet_inv_dash/page/dropletdashboard/dropletdashboard.js
+++ droplet_inv_dash/droplet_inv_dash/page/dropletdashboard/dropletdashboard.js
@@ -1,8 +1,33 @@
 frappe.pages['dropletdashboard'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Dashboard',
-		single_column: true
-	});
-	let $btn = page.set_primary_action('View Dashboard', () => console.log("GOOOOO"), 'octicon octicon-plus');
+	new DropDash(wrapper);
+}
+
+// PAGE CONTENT
+DropDash = Class.extend({
+	init: function (wrapper) {
+		this.page = frappe.ui.make_app_page({
+			parent: wrapper,
+			title: 'Droplet Dashboard',
+			single_column: true
+		});
+		// Make Page
+		this.make();
+	},
+	//Make Page Function
+	make: function () {
+		// Grab the Class
+		let me = $(this);
+
+		// Push DOM element to page
+		$(frappe.render_template(frappe.droplet_dash_page.body, this)).appendTo(this.page.main);
+		window.location.href = frappe.urllib.get_full_url("/special");
+
+	}
+	// End of Class
+})
+
+// HTML Content
+let body = '<h1>Opening Dashboard...</h1>';
+frappe.droplet_dash_page =  {
+	body: body
 }
