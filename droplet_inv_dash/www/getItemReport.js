3cec568fb18c2b949d21d5c04f103f4a4f347db2
sanderson162 <sanderson162@my.bcit.ca>
2022-03-31 11:16:24 -0700

    changed amount to qty for items

--- droplet_inv_dash/www/getItemReport.js
+++ droplet_inv_dash/www/getItemReport.js
@@ -271,13 +271,13 @@ async function getItemReportFromDatabase() {
       delivery_date.setDate(delivery_date.getDate() - item_lead_time);
       // get the details of the bom given the name in the item
       if(!item_order.hasOwnProperty('bom_no')){
-        item_report_list.pushCount(item_order.item_code, parseInt(item_order.amount), delivery_date);
+        item_report_list.pushCount(item_order.item_code, parseInt(item_order.qty), delivery_date);
       } else {
         const bomDetails = await cache.request(`resource/BOM/${item_order.bom_no}`, getFrappeJson);
         for (const key in bomDetails.items) {
           const item = bomDetails.items[key];
           // push the required amount of items to the list, if the item doesnt exist it will be added
-          item_report_list.pushCount(item.item_code, parseInt(item.amount * item_order.amount), delivery_date);
+          item_report_list.pushCount(item.item_code, parseInt(item.qty * item_order.qty), delivery_date);
         }
       }
     }
