110cc6c31a805049677e374f59a36e3c2c5b436f
Tommy Nguyen <tommylulxdd@gmail.com>
2023-05-25 23:55:00 +0000

    Quick readjust to colour for items

--- droplet_inv_dash/www/getItemReport.js
+++ droplet_inv_dash/www/getItemReport.js
@@ -427,20 +427,20 @@ class Item_report {
     }
 
     // set flag based off of distance to order date from current date
-    if (this.order_date_formatted == "N/A" || this.to_order[to_order_month][1] == 0 || this.order_date == null) {
-      this.flag = "green";
+    if (this.order_date_formatted == "N/A" || this.order_date == null) {
+      this.flag = "white";
     } else {
       let daysUntilOrder = getDaysBetweenDates(this.server_date, this.order_date);
       // console.log(this.to_order[this.order_date.getMonth()][1])
-      if (daysUntilOrder <= 1) {
+      if (daysUntilOrder <= 1 && this.to_order[to_order_month][1] != 0) {
         this.flag = "red";
-      } else if (daysUntilOrder < 7) {
+      } else if (daysUntilOrder < 7 && this.to_order[to_order_month][1] != 0) {
         this.flag = "orange";
-      } else if (daysUntilOrder < 14) {
+      } else if (daysUntilOrder < 14 && this.to_order[to_order_month][1] != 0) {
         this.flag = "yellow";
-      } else if (daysUntilOrder < 21) {
+      } else if (daysUntilOrder < 21 && this.to_order[to_order_month][1] != 0) {
         this.flag = "lightgreen";
-      } else if (daysUntilOrder < 35) {
+      } else if (daysUntilOrder < 35 && this.to_order[to_order_month][1] == 0) {
         this.flag = "green";
       } else {
         this.flag = "green";
